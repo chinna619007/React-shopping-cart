1cdc32ff167d37173e54055575a949e07e0d6ea7
chinnadurai <chinna619007@gmail.com>
2022-11-13 07:21:02 +0530

    project initial style setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header>
+        <a href="/">Shopping Cart</a>
+      </header>
+      <main>Product List</main>
+      <footer>E-commerce on React</footer>
+    </div>
+  );
+}
+
+export default App;
